bf58582f65f790fa483c3a73d65176b54ae00913
SayedAtef <sayedatef6293@gmail.com>
2021-10-12 15:43:43 +0200

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=sayed
+export POSTGRES_PASSWORD=0020663602364
+export POSTGRES_HOST=cdr.cd7xmb63rkra.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::test-590048555026-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
