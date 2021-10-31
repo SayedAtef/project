9558b95b5221c638bcf3598f582c47a4d86f0087
SayedAtef <sayedatef6293@gmail.com>
2021-10-31 09:32:43 +0200

    added screenshots

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://a3f3da0f679b34c57938a8b6d0bf362a-1764342842.us-east-2.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
